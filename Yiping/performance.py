d81aba6a1bba521bf971bf42d0113fdcc7b977b2
CMC2002 <cmc.2002.tw@gmail.com>
2022-12-19 18:42:22 +0800

    add performance

--- Yiping/performance.py
+++ Yiping/performance.py
@@ -12,6 +12,7 @@ import pandas as pd
 import Functions as func
 import torch.optim as optim
 
+device = "cuda"
 model = torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', weights= 'DEFAULT')
 
 classifier = nn.Sequential(
@@ -21,13 +22,58 @@ model.fc = classifier
 
 model = model.to(device)
 opt = optim.AdamW(model.parameters(), lr= 0.001)
-model, _= func.loadmodel("/home/meng/Yiping/model/checkpoint.ckpt", model, opt, device= device)
+model, _= func.loadmodel("/home/meng/Yiping/model/checkpoint_n32.ckpt", model, opt, device= device)
 
+
+trans_v = transforms.Compose([transforms.Resize([300, 300], interpolation=2),
+              transforms.ToTensor(),
+              transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+validset = torchvision.datasets.PCAM(root="YiPing", split = 'val', download= True, transform= trans_v)
 loader = torch.utils.data.DataLoader(dataset = validset, batch_size = 1, shuffle = False)
 
 model.eval()
-accuracy = []
-output = []
+outputs = []
+GT = []
+accuracy = 0
 
 from tqdm import tqdm
-for data, target in tdqm(loader):
+for data, target in tqdm(loader):
+    with torch.no_grad():
+        output = model(data.to(device))
+        _, predicted = torch.max(output.data, 1)
+        
+        GT.append(target.cpu().numpy()[0])
+        accuracy += (predicted == target.to(device)).sum().item()
+        outputs.append(predicted.cpu().numpy()[0])
+accuracy /= len(loader)
+
+import matplotlib as plt
+GT = np.array(GT, dtype= int)
+outputs = np.array(outputs, dtype= int)
+
+from numpy import save
+save("groundtruth.npy", GT)
+save("outputs.npy", outputs)
+
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+print("AUC= ", round(roc_auc_score(outputs, GT),3))
+fpr, tpr, thresholds = roc_curve(outputs, GT, pos_label= 1)
+plt.plot(fpr, tpr, color= 'red', label= 'ROC curve (area = %0.2f)')
+plt.grid(color= 'grey', linewidth= 0.5)
+plt.plot([0,1], [0,1], color= 'green', linestyle= '--')
+plt.show()
+
+from sklearn.metrics import accuracy_score
+print("Accuracy= ", round(accuracy_score(outputs, GT), 3))
+
+from sklearn.metrics import confusion_matrix
+cnf = confusion_matrix(outputs, GT)
+tp = cnf[0][0]
+fp = cnf[0][1]
+tn = cnf[1][1]
+fn = cnf[1][0]
+print("Sensitivity= ", round(tp/(tp+fn), 3))
+print("Specificity= ", round(tn/(tn+fp), 3))
+print("Precision= ", round(tp/(tp+fp), 3))
+print("Negative Predictive Value= ", round(tn/(tn+fn), 3))
